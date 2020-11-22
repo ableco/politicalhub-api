fb5a43044dca5f94041c6dad24a0895a23e90f20
Cristian Granda <cristian.granda.pastor@gmail.com>
2020-11-21 22:15:28 -0500

    Add Content-Type application/json to headers

--- app.rb
+++ app.rb
@@ -3,14 +3,17 @@ require "cuba"
 Cuba.define do
   on get do
     on "organizaciones-politicas" do
+      res.headers["Content-Type"] = "application/json"
       res.write File.read("./data/organizaciones-politicas.json")
     end
 
     on "congresistas/:identifier" do |identifier|
+      res.headers["Content-Type"] = "application/json"
       res.write File.read("./data/congresistas-2020/#{identifier}.json")
     end
 
     on "congresistas" do
+      res.headers["Content-Type"] = "application/json"
       res.write File.read("./data/congresistas-2020.json")
     end
   end
