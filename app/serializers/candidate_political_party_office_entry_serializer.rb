7214c1f18f2fc350a32461272e0ff3f8028fc4de
hpneo <hpneo@hotmail.com>
2021-03-09 19:35:58 -0500

    Add political_organization_id to CandidatePoliticalPartyOfficeEntrySerializer and CandidatePreviousElectedOfficeEntrySerializer

--- app/serializers/candidate_political_party_office_entry_serializer.rb
+++ app/serializers/candidate_political_party_office_entry_serializer.rb
@@ -1,5 +1,5 @@
 class CandidatePoliticalPartyOfficeEntrySerializer < ActiveModel::Serializer
-  attributes :political_organization, :office, :start_year, :end_year
+  attributes :political_organization, :political_organization_id, :office, :start_year, :end_year
 
   def political_organization
     object.political_organization_name
--- app/serializers/candidate_previous_elected_office_entry_serializer.rb
+++ app/serializers/candidate_previous_elected_office_entry_serializer.rb
@@ -1,5 +1,5 @@
 class CandidatePreviousElectedOfficeEntrySerializer < ActiveModel::Serializer
-  attributes :political_organization, :office, :start_year, :end_year, :comments
+  attributes :political_organization, :political_organization_id, :office, :start_year, :end_year, :comments
 
   def political_organization
     object.political_organization_name
